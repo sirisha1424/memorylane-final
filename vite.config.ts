7271ea20c02ee57644aa57015bf7da5f25311724
Esha Gupta <esha34gupta@gmail.com>
2024-09-15 18:40:08 +0530

    doing changes1

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // This changes the out put dir from dist to build
-  // comment this out if that isn't relevant for your project
+  base: './',  // Ensures relative paths
   build: {
-    outDir: "dist",
+    outDir: 'dist',
+    rollupOptions: {
+      input: '/path/to/index.html',
+    },
   },
-  plugins: [tsconfigPaths(), react()],
 });
