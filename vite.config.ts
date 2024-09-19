0e6f4dc3e2d5713062c16a9aa12f46cd43b3f13c
Esha Gupta <esha34gupta@gmail.com>
2024-09-19 21:39:49 +0530

    small changes1

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: "dist",  // Align this with your vercel.json
+    outDir: "out",  // Align this with your vercel.json
   },
   plugins: [tsconfigPaths(), react()],
 });
